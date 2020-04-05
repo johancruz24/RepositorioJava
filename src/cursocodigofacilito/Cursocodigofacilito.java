09a0008a0c44e73d853b04b2af0f080577a1cf87
Johan Cruz <sebastiancruzsanchez@gmail.com>
2020-04-05 12:28:53 -0500

    Se ha creado proyecto para primer repositorio

---
+++ src/cursocodigofacilito/Cursocodigofacilito.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursocodigofacilito;
+
+/**
+ *
+ * @author CruzArdila
+ */
+public class Cursocodigofacilito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Primer repositorio en GitHub");
+        // TODO code application logic here
+    }
+    
+
+    
+}
